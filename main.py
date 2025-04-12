4fa5c316edaf5ad5ed5f8dbee12c946c57d6c353
Nelson Meas <emailnelsonm@gmail.com>
2025-04-11 23:41:52 -0700

    1st

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import random
 def main():
     total = 0
     numbers = []
+
+    while total < 100:
+        randnum = random.randint(1,10)
+        if total + randnum > 100:
+            break
+        total += randnum
+        numbers.append(randnum)
+
+    
+
     """
     ########################################
     Code Your Program here
